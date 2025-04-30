d5ccbfe374cd521c857ca5c01813f82ab94de9fa
awatersny <awatersny@gmail.com>
2025-04-30 18:20:46 -0400

    Reformat date

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,8 +1,9 @@
 export default function Score({date, score}: {date: string, score: number}) {
   return (
     <div className='score'>
+      {`${date.slice(5)}-${date.slice(0, 4)}`}
       <div><b>Score: </b>{score}</div>
-      <em>{date}</em>
+      
     </div>
   )
 }
